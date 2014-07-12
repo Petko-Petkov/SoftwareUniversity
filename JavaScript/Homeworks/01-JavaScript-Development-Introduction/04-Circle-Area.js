6486fafc004889febf19c71c7fb5fd41a58d4a89
petko-petkov <petko.hr.petkov@gmail.com>
2014-07-12 17:15:44 +0300

    Restructuring.

--- JavaScript/Homeworks/01-JavaScript-Development-Introduction/04-Circle-Area.js
+++
@@ -1,4 +0,0 @@
-function calcCircleArea () {
-	var radius = document.getElementById('radius').value;
-	document.body.innerHTML += "<br/>r = " + radius + "; area = " + (Math.PI*Math.pow(radius, 2)) + "<br/>";
-}
